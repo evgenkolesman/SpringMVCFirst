3d131bf01ed5cd7620142e9767aaa981cfdcaadd
glavs <glavstroi_e@mail.ru>
2021-07-29 21:35:37 +0300

    Spring MVC

--- src/main/java/springstore/controllers/FirstController.java
+++ src/main/java/springstore/controllers/FirstController.java
@@ -3,18 +3,25 @@ package springstore.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
-//@RequestMapping("/first")
+@RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name",required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+//        String name = request.getParameter("name");
+//        String surname = request.getParameter("surname");
+        System.out.println("Hello " + name + " " + surname);
         return "first/hello";
     }
 
-    @GetMapping("/goodbye")
+    @GetMapping("/goodBye")
     public String goodByePage() {
-        return "first/goodbye";
+        return "first/goodBye";
     }
 }
