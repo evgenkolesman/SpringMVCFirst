ac509775b595acf3dd1dc0c5707d144c56a32beb
glavs <glavstroi_e@mail.ru>
2021-07-30 17:19:27 +0300

    Spring MVC Модель. Передача данных от контроллера к представлению.

--- src/main/java/springstore/controllers/FirstController.java
+++ src/main/java/springstore/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package springstore.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,16 +13,41 @@ import javax.servlet.http.HttpServletRequest;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(@RequestParam(value = "name",required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
-//        String name = request.getParameter("name");
-//        String surname = request.getParameter("surname");
-        System.out.println("Hello " + name + " " + surname);
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname, Model model) {
+        //System.out.println("Hello " + name + " " + surname);
+        model.addAttribute("message", "Hello " + name + " " + surname);
         return "first/hello";
     }
 
     @GetMapping("/goodBye")
     public String goodByePage() {
         return "first/goodBye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a", required = false) int a,
+                             @RequestParam(value = "b", required = false) int b,
+                             @RequestParam(value = "operation", required = false) String operation, Model model) {
+        double z;
+        switch (operation) {
+            case "multy":
+                z = a * b;
+                break;
+            case "div":
+                z = a / (double) b;
+                break;
+            case "add":
+                z = a + b;
+                break;
+            case "sub":
+                z = a - b;
+                break;
+            default:
+                z = 0;
+
+        }
+        model.addAttribute("message", z);
+        return "first/calculator";
     }
 }
