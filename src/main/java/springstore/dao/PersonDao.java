3d130c29839fc36845a05eb244d7861f971fd1ae
glavs <glavstroi_e@mail.ru>
2021-08-12 15:01:45 +0300

    Spring MVC CRUD приложение добавил валидацию добавил загрузку в БД Работа с JDBC Teamplate

--- src/main/java/springstore/dao/PersonDao.java
+++ src/main/java/springstore/dao/PersonDao.java
@@ -1,88 +1,44 @@
 package springstore.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import springstore.models.Person;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDao {
-     private static final String URL = "jdbc:postgresql://localhost:5432/FirstDB";
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "PassworD1";
 
-    private static Connection connection;
+    private final JdbcTemplate jdbcTeamplate;
 
-    static {
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+    @Autowired
+    public PersonDao(JdbcTemplate jdbcTeamplate) {
+        this.jdbcTeamplate = jdbcTeamplate;
     }
 
     public List<Person> index() {
-        List<Person> people = new ArrayList<>();
-
-        try {
-            Statement statement = connection.createStatement();
-            String SQL = "SELECT * FROM Person";
-            ResultSet resultSet = statement.executeQuery(SQL);
-
-            while (resultSet.next()) {
-                Person person = new Person();
-
-                person.setId(resultSet.getInt("id"));
-                person.setName(resultSet.getString("name"));
-                person.setEmail(resultSet.getString("email"));
-                person.setAge(resultSet.getInt("age"));
-
-                people.add(person);
-            }
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
-        return people;
+        return jdbcTeamplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
     }
 
     public Person show(int id) {
-//        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
-        return null;
+        return jdbcTeamplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id},
+                new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny().orElse(null);
     }
 
     public void save(Person person) {
-        try {
-            Statement statement = connection.createStatement();
-            String SQL = String.format("INSERT INTO Person VALUES(%d, '%s', %d, '%s')",
-                    person.getId(), person.getName(), person.getAge(), person.getEmail());
-
-            statement.executeUpdate(SQL);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
-
+        jdbcTeamplate.update("INSERT INTO Person VALUES (?, ?, ?, ?)",
+                person.getId(), person.getName(), person.getAge(), person.getEmail());
     }
 
     public void update(int id, Person updatedPerson) {
-//        Person personToBeUpdated = show(id);
-//
-//        personToBeUpdated.setName(updatedPerson.getName());
-//        personToBeUpdated.setAge(updatedPerson.getAge());
-//        personToBeUpdated.setEmail(updatedPerson.getEmail());
+        jdbcTeamplate.update("UPDATE Person set name = ?, age = ?, email = ? WHERE id =?;",
+                updatedPerson.getName(), updatedPerson.getAge(), updatedPerson.getEmail(), id);
     }
 
     public void delete(int id) {
-//        people.removeIf(p -> p.getId() == id);
+        jdbcTeamplate.update("DELETE FROM Person WHERE id = ?;", id);
     }
 }
